4f6ae9c8cd37fc82f6925712bd5d726b0fd51cf1
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-01-22 17:09:32 +0300

    Set login activity as start up activity.

--- app/src/main/java/com/github/handioq/diberapp/ui/auth/login/LoginActivity.java
+++ app/src/main/java/com/github/handioq/diberapp/ui/auth/login/LoginActivity.java
@@ -3,14 +3,14 @@ package com.github.handioq.diberapp.ui.auth.login;
 import android.os.Bundle;
 
 import com.github.handioq.diberapp.R;
-import com.github.handioq.diberapp.base.BaseActivity;
+import com.github.handioq.diberapp.base.BaseToolbarActivity;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseToolbarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawer);
+        setContentView(R.layout.activity_toolbar);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
