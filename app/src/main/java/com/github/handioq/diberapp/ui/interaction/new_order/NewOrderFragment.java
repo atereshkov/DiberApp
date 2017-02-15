d40a9912277f6ce4ece6a7a8ddb4d4a75073b38d
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-02-15 21:39:34 +0300

    Added stub textChanged listener for address edit text

--- app/src/main/java/com/github/handioq/diberapp/ui/interaction/new_order/NewOrderFragment.java
+++ app/src/main/java/com/github/handioq/diberapp/ui/interaction/new_order/NewOrderFragment.java
@@ -2,6 +2,8 @@ package com.github.handioq.diberapp.ui.interaction.new_order;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -76,6 +78,24 @@ public class NewOrderFragment extends BaseFragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.select_dialog_singlechoice, shops);
         addressFromEditView.setThreshold(2);
         addressFromEditView.setAdapter(adapter);
+
+        addressFromEditView.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // todo send query to server or smth
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
 
